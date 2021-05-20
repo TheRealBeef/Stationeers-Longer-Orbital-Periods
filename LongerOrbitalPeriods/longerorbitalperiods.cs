c2df8a0bf0267553255fae79b81bc831d8cf26b9
TheRealBeef <michael.d.friske@outlook.com>
2021-05-20 09:23:38 +0200

    Add project files.

---
+++ LongerOrbitalPeriods/longerorbitalperiods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+
+namespace LongerOrbitalPeriods
+{
+    public class Orb_Period
+    {
+
+        [BepInPlugin("org.bepinex.plugins.orbitalperiod", "Beef's Longer Orbital Periods", "0.1")]
+        [BepInProcess("rocketstation.exe")]
+        public class init : BaseUnityPlugin
+        {
+            void Awake()
+            {
+                OrbitalPeriod.Awake();
+            }
+        }
+
+        public class OrbitalPeriod
+        {
+            // Variable Definitions
+
+            public static void AppendLog(string logdetails)
+            {
+                UnityEngine.Debug.Log("Beef's Longer Orbital Periods - " + logdetails);
+            }
+
+            // Awake is called once when both the game and the plug-in are loaded
+            public static void Awake()
+            {
+                //Initialize();
+
+                AppendLog("Initialized");
+                var harmony = new Harmony("org.bepinex.plugins.orbitalperiod");
+                harmony.PatchAll();
+                AppendLog("Patched with Harmony");
+            }
+        }
+    }
+
+}
---
+++ LongerOrbitalPeriods/patches.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using Assets.Scripts;
+
+namespace OrbitalPeriod
+{
+	
+	// Fixes to SSAO
+	[HarmonyPatch(typeof(CursorManager), "AssignSun")]
+    public class OrbitalPatcher
+    {
+        static void Prefix()
+        {
+            // Current multiplier is 10
+            // Orbital Period of 0.1 will be 20 minutes
+            // Orbital period of 1.0 will be 3 hours 20 minutes
+            // Orbital Period of 2.0 will be 6 hours 40 minutes
+            
+            float BaseOrbitalPeriodMultiplier = 10.0f;
+
+            LongerOrbitalPeriods.Orb_Period.OrbitalPeriod.AppendLog("Applying Orbital Period Settings");
+
+            CursorManager.Instance.BaseSunOrbitPeriod *= BaseOrbitalPeriodMultiplier;
+
+            LongerOrbitalPeriods.Orb_Period.OrbitalPeriod.AppendLog("Orbital Period Set - New settings are " + BaseOrbitalPeriodMultiplier.ToString() + "x longer");
+        }
+    }
+}
